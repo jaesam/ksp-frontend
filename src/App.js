4dfae06b18af0c28e52815199aa624d4f79df424
jaesam <choiubc@gmail.com>
2020-05-31 21:55:07 +0900

    Add sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <article className="App">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Fetus version of KSP Website
@@ -18,8 +17,24 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */}
+      <section className="bgv-container">
+        <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" autoPlay controls />
+      </section>
+      <section className="section2">
+        <p>
+          거래처
+        </p>
+      </section>
+      <section className="section3">
+        <p>
+          강점
+        </p>
+      </section>
+      <footer>
+        <h1>Contact Us</h1>
+      </footer>
+    </article>
   );
 }
 
