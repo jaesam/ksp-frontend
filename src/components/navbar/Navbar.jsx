29b16b66b5851686731ded1b4e5a31850ab7e56c
jaesam <choiubc@gmail.com>
2020-06-01 22:37:08 +0900

    Add nav animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import classnames from "classnames";
 
 function Navbar(props) {
-    return (
-        <nav className="navbar">
-            <ul className="navbar-nav"> {props.children} </ul>
-        </nav>
-    )
+  const [activeNav, setActiveNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
+  function onScroll() {
+    const viewportHeight = window.innerHeight * 0.1;
+    const currentYOffset = window.pageYOffset;
+
+    setActiveNav(viewportHeight < currentYOffset);
+  }
+
+  return (
+    <nav
+      className={classnames("navbar", {
+        "navbar--hidden": !activeNav
+      })}
+    >
+      <ul className="navbar-nav"> {props.children} </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
