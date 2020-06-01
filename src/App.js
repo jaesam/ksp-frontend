8b4f1e8860d2927b18b999789c834b223b311e54
jaesam <choiubc@gmail.com>
2020-06-01 19:16:16 +0900

    TODO: clean props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import { ReactComponent as Logo } from './icons/airbnb.svg';
+
 // create index.js to export as a directory for refactoring
 import NavItem from './components/navbar/NavItem';
 import DropdownMenu from './components/navbar/DropdownMenu';
 import Navbar from './components/navbar/Navbar';
 
+
 function App() {
   return (
     <article>
       <Navbar>
+        <Logo />
         <NavItem icon="회사소개" />
         <NavItem icon="제품소개" />
         <NavItem icon="Contact" />
@@ -20,12 +24,12 @@ function App() {
       <section className="bgv-container">
         <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" autoPlay />
       </section>
-      <section className="section2">
+      <section className="section1">
         <p>
           거래처
         </p>
       </section>
-      <section className="section3">
+      <section className="section2">
         <p>
           강점
         </p>
