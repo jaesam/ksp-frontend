26724093e01afc2ca4869f70f12283591ba3c6f6
jaesam <choiubc@gmail.com>
2020-06-01 20:59:22 +0900

    Check up

--- src/components/navbar/DropdownMenu.jsx
+++ src/components/navbar/DropdownMenu.jsx
@@ -1,58 +1,61 @@
-import React, { useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import React, { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState();
 
-    const [activeMenu, setActiveMenu] = useState('main');
-    const [menuHeight, setMenuHeight] = useState();
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
 
-    function calcHeight(el) {
-        const height = el.offsetHeight;
-        setMenuHeight(height);
-    }
-
-    function DropdownItem(props) {
-        return (
-            <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-                <span className="icon-button">{props.leftIcon}</span>
-                {props.children}
-                <span className="icon-right">{props.rightIcon}</span>
-            </a>
-        )
-    }
-
+  function DropdownItem(props) {
+    /* eslint-disable */
     return (
-        <div className="dropdown" style={{ height: menuHeight }}>
-            <CSSTransition
-                in={activeMenu === 'main'}
-                unmountOnExit
-                timeout={500}
-                classNames="menu-primary"
-                onEnter={calcHeight}>
-                <div className="menu">
-                    <DropdownItem>회사소개</DropdownItem>
-                    <DropdownItem>제품소개</DropdownItem>
-                    <DropdownItem
-                        leftIcon="😃"
-                        rightIcon="!"
-                        goToMenu="settings">
-                        Contact
-            </DropdownItem>
-                </div>
-            </CSSTransition>
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
 
-            <CSSTransition
-                in={activeMenu === 'settings'}
-                unmountOnExit
-                timeout={500}
-                classNames="menu-secondary">
-                <div className="menu">
-                    <DropdownItem leftIcon="LEFTICON" goToMenu="main" />
-                    <DropdownItem>Settings</DropdownItem>
-                </div>
-            </CSSTransition>
+  return (
+    <div className="dropdown" style={{ height: menuHeight }}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>회사소개</DropdownItem>
+          <DropdownItem>제품소개</DropdownItem>
+          <DropdownItem leftIcon="😃" rightIcon="!" goToMenu="settings">
+            Contact
+          </DropdownItem>
         </div>
-    )
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "settings"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+      >
+        <div className="menu">
+          <DropdownItem leftIcon="LEFTICON" goToMenu="main" />
+          <DropdownItem>Settings</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
 }
 
 export default DropdownMenu;
